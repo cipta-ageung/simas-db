6f1e6f729faa111e692d34239bb7eb99f96ba3b5
ciptaageung <cipta_ageung@yahoo.co.id>
2020-01-04 18:50:10 +0700

    add comment service db

--- services/db.go
+++ services/db.go
@@ -15,7 +15,7 @@ type ServiceConnection struct{}
 func (g *ServiceConnection) SetupDb(ctx context.Context, req *pgdb.ServiceApp, rsp *pgdb.ServiceDb) error {
 
 	// clear or empty first for new request
-	rsp = &pgdb.ServiceDb{}
+	//rsp = &pgdb.ServiceDb{}
 
 	// load data from file configuration
 	config.LoadFile("./config/db.json")
@@ -25,12 +25,13 @@ func (g *ServiceConnection) SetupDb(ctx context.Context, req *pgdb.ServiceApp, r
 
 	// check data struct or response
 	if rsp != nil {
-		log.Print("setup db for services : " + req.Svc)
 
 		// set data response string connection
 		rsp.ConnectionDb = "postgres://" + rsp.GetUser() + ":" + rsp.GetPassword() + "@" + rsp.GetHost() +
 			":" + rsp.GetPort() + "/" + rsp.GetDbname() + "?sslmode=disable"
 	}
+
+	log.Print(rsp)
 
 	return nil
 }
